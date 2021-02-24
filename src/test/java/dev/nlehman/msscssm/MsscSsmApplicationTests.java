051b805e53c1810417eb7574c2144b4684bc5dbe
Nathan Lehman <nlehman@dlcouch.com>
2021-02-24 08:51:59 -0500

    init project

---
+++ src/test/java/dev/nlehman/msscssm/MsscSsmApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.nlehman.msscssm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscSsmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
